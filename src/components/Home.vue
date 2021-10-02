b6ea41b4a8d76d13a12903496fba39fa42d26a99
audetcameron <audetcameron@gmail.com>
2021-10-01 22:27:27 -0400

    testing functions and identity

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,16 @@
 <template>
-    <div>Home</div>
+    <div>
+    
+    <p>Hello {{ username }} Whats going on?</p>
+    
+    </div>
 </template>
 
 <script>
     export default {
+    props: [
+        "username"
+    ],
         name:"Home"
     }
 </script>
